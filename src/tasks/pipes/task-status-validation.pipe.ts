74bd63c638245ae97aa9991f24e96238ccc5cb61
Park.Lu <park.lu@nogle.com>
2020-07-27 11:13:30 +0800

    add validation pipes

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,16 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  transform(value: any): any {
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is an invalid status`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return status in TaskStatus;
+  }
+}
